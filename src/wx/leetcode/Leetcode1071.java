551879811eae85e1e6f0505ebfaea99c5bfe3fe8
wx <421980589@qq.com>
2020-03-11 22:17:39 -0700

    string

---
+++ src/wx/leetcode/Leetcode1071.java
@@ -0,0 +1,42 @@
+package wx.leetcode;
+
+public class Leetcode1071 {
+
+    /**
+     * 1071. Greatest Common Divisor of Strings
+     * For strings S and T, we say "T divides S" if and only if S = T + ... + T  (T concatenated with itself 1 or more times)
+     *
+     * Return the largest string X such that X divides str1 and X divides str2.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: str1 = "ABCABC", str2 = "ABC"
+     * Output: "ABC"
+     * Example 2:
+     *
+     * Input: str1 = "ABABAB", str2 = "ABAB"
+     * Output: "AB"
+     * Example 3:
+     *
+     * Input: str1 = "LEET", str2 = "CODE"
+     * Output: ""
+     *
+     */
+    class Solution {
+        public String gcdOfStrings(String str1, String str2) {
+            // 假设str1是N个x，str2是M个x，那么str1+str2肯定是等于str2+str1的。
+            if (!(str1 + str2).equals(str2 + str1)) {
+                return "";
+            }
+            // 辗转相除法求gcd。
+            return str1.substring(0, gcd(str1.length(), str2.length()));
+        }
+
+        private int gcd(int a, int b) {
+            return b == 0? a: gcd(b, a % b);
+        }
+
+    }
+}
