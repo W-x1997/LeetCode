ad3fdf450107753383fa4520bd62e7e7e1cb554a
wx <421980589@qq.com>
2020-05-06 17:40:04 -0700

    add2

--- src/wx/leetcode/LeetCode2.java
+++ src/wx/leetcode/LeetCode2.java
@@ -31,6 +31,40 @@ public class LeetCode2 {
 
     class Solution {
 
+        public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+
+            ListNode dummy=new ListNode(0);
+            ListNode cur=dummy;
+            int carry=0;
+            int sum=0;
+
+            while(l1!=null||l2!=null||carry!=0){
+                if(l1==null&&l2==null)
+                    sum=carry;
+                else if(l2==null)
+                    sum=l1.val+carry;
+                else if(l1==null)
+                    sum=l2.val+carry;
+                else
+                    sum=l1.val+l2.val+carry;
+                if(sum>=10){
+                    sum=sum%10;
+                    carry=1;
+                }else {
+                    carry=0;   ///!!!!!carry要恢复为0
+                }
+                cur.next=new ListNode(sum);
+                cur=cur.next;
+
+                if(l1!=null)   //l1可能为空
+                    l1 = l1.next;
+                if(l2!= null)
+                    l2=l2.next;
+            }
+            return dummy.next;
+
+        }
+
         public class ListNode {
             int val;
             ListNode next;
