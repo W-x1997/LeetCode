d7a8c83e601492f3dfbd54991a78b06967cccc61
wx <421980589@qq.com>
2020-05-28 17:37:21 +0800

    937. Reorder Data in Log Files --custom sort

--- src/wx/leetcode/Leetcode937.java
+++ src/wx/leetcode/Leetcode937.java
@@ -7,6 +7,12 @@ public class Leetcode937 {
     class Solution {
         /**
          937. Reorder Data in Log Files
+
+
+         这个是sort方法里的那个比较函数的特殊的返回值，比较函数的参数 a 和 b 代表数组里的两个元素，
+         如果 a 小于 b，排序后的数组 a 在 b 前面，此时返回一个负数 （即上面的 return -1）
+         如果 a 等于 b，排序后两者位置不变，此时返回 0 （即上面的 return 0）
+         如果 a 大于 b，排序后 a 在 b 后面， 返回一个正数 （即上面的 return 1）
          **/
         public String[] reorderLogFiles(String[] logs) {
             if(logs==null||logs.length==0)return logs;
