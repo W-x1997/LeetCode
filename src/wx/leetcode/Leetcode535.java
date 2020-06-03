795b4f5e873c1452c900dbed4e40b6b5c3e955f7
wx <421980589@qq.com>
2020-06-03 15:34:22 +0800

    535. Encoded and Decode TinyURL

---
+++ src/wx/leetcode/Leetcode535.java
@@ -0,0 +1,30 @@
+package wx.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leetcode535 {/**
+
+ 535. Encode and Decode TinyURL
+
+
+ Share
+ Note: This is a companion problem to the System Design problem: Design TinyURL.
+ TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+
+ Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+ **/
+    public class Codec {
+        Map<Integer, String> map = new HashMap<>();
+        int i = 0;
+
+        public String encode(String longUrl) {
+            map.put(i, longUrl);
+            return "http://tinyurl.com/" + i++;
+        }
+
+        public String decode(String shortUrl) {
+            return map.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+        }
+    }
+}
