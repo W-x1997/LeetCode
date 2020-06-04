086416165dd0bc360c029cf540d38a5a44d5b181
wx <421980589@qq.com>
2020-06-04 21:48:33 +0800

    680. Valid Palindrome II

--- src/wx/leetcode/Leetcode680.java
+++ src/wx/leetcode/Leetcode680.java
@@ -7,6 +7,34 @@ public class Leetcode680 {
      */
 
     class Solution {
+
+        /**
+         * 双指针 或者 递归
+         * @param s
+         * @return
+         */
+        public boolean validPalindrome2(String s) {
+            boolean removed=false;
+            int i = 0, j = s.length()-1;
+            while (i < j) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    if (removed) {
+                        return false;
+                    } else {
+                        removed = true;
+                        if (validPalindrome(s.substring(i+1, j+1)) || validPalindrome(s.substring(i, j)))
+                            return true;
+                        else
+                            return false;
+                    }
+                }
+
+                i++;
+                j--;
+            }
+
+            return true;
+        }
         public boolean isPalindromeRange(String s, int i, int j) {
             for (int k = i; k <= i + (j - i) / 2; k++) {
                 if (s.charAt(k) != s.charAt(j - k + i)) return false;
