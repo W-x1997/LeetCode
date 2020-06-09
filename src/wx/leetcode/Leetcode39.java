a5d83ea193759a434e20084e44a0da6ce416bed8
weixin <Weixin.xwei3541@bytedance.com>
2020-06-09 23:47:45 +0800

    39. Combination Sum---DFS

--- src/wx/leetcode/Leetcode39.java
+++ src/wx/leetcode/Leetcode39.java
@@ -9,10 +9,18 @@ public class Leetcode39 {
 
          The same repeated number may be chosen from candidates unlimited number of times.
          **/
+
+
         public List<List<Integer>> combinationSum(int[] candidates, int target) {
             List<List<Integer>> res=new ArrayList();
 
-
+            /**
+             * 这里注意最后一个参数 dfs中从i开始
+             *
+             * 给定candidates中无重复的，所以不用sort也不用判断临近的两个数是否相等(nums[i]==nums[i-1] continue)
+             * 但是当前index的数字可以选用无数次，所以call 下一次的时候要 传入i，而不是i+1.
+             * 只有在remain==0的时候才添加到res，start可以小于等于nums.length
+             */
             dfs(res,new ArrayList(),candidates,target,0,0);
 
 
