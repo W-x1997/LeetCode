adbab21dabdcd615ebc08a0e17d64c6cbe88e4f4
weixin <Weixin.xwei3541@bytedance.com>
2020-06-19 16:41:08 +0800

    938. Range Sum of BST

---
+++ src/wx/leetcode/Leetcode938.java
@@ -0,0 +1,54 @@
+package wx.leetcode;
+
+public class Leetcode938 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode() {}
+            TreeNode(int val) { this.val = val; }
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        public int rangeSumBST(TreeNode root, int L, int R) {
+            if(root==null)
+                return 0;
+
+            if(root.val>R){
+                return rangeSumBST(root.left,L,R);
+            }
+            if(root.val<L){
+                return rangeSumBST(root.right,L,R);
+            }
+
+
+            return root.val+rangeSumBST(root.right,L,R)+rangeSumBST(root.left,L,R);
+
+
+        }
+
+        /**
+         * Definition for a binary tree node.
+
+          }
+         */
+    }
+    }}
